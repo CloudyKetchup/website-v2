a2e7747ca23d0779befc49c3f827c890921028b8
CloudyKetchup <maxdodon25@gmail.com>
2021-03-27 01:30:55 +0000

    replaced video with background image

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -10,20 +10,12 @@ export default function Landing() {
 	
 	return (
 		<section className="h-screen w-screen relative">
-			<video
-				id="landing-video"
+			<img
 				className="filter-brightness object-cover h-full w-full bg-cover bg-no-repeat absolute z-0"
-				muted
-				loop
-				autoPlay
-				controls={false}
-				src="/videos/tokyo.mp4"
-				type="video/mp4"
-				disablePictureInPicture
-				disableRemotePlayback
+				src="/images/background.png"
 			/>
 			<div className="flex content-middle h-full w-full absolute z-10 p-16 pt-28">
-				<h2 className="m-auto text-white text-4xl font-bold text-wrap">
+				<h2 className="text-center m-auto text-white text-4xl font-bold text-wrap">
 					Building the web of the future
 				</h2>
 			</div>
