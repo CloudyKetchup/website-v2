d3757064cf60ccfa62f1e42a3571a08dd456eee5
CloudyKetchup <maxdodon25@gmail.com>
2021-03-27 01:31:17 +0000

    text center when responsive

--- src/components/sections/Intro/index.js
+++ src/components/sections/Intro/index.js
@@ -4,10 +4,10 @@ export default function Intro() {
   return (
     <section className="bg-white w-screen p-10 md:p-20 shadow-inner">
       <div className="flex-1 md:flex h-full justify-center content-middle m-auto content-middle space-x-0 md:space-x-24 space-y-20 md:space-y-0 pb-20 md:pb-none">
-        <div className="mt-auto mb-auto flex h-full w-full md:max-w-sm" >
-          <p className="m-auto mb-auto text-4xl font-bold tracking-wide break-word">
+        <div className="mt-auto mb-auto flex h-full w-full md:max-w-md" >
+          <p className="m-auto mb-auto text-2xl font-bold tracking-wide text-center md:text-left break-word">
             <span className="pl-10 md:pl-0" /> Hi, my name is Maxim, I'm a Frontend developer
-          focusing on the creation of modern websites and single page applications with
+          focusing on the creation of modern websites and single page web applications with
           <span className="text-blue-400"> React</span>. <br />
           Trying to build the web which we all deserve.
         </p>
