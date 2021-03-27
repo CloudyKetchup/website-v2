51f2daf62d3f42ecb5a0659443ee0ae9fa6fc434
CloudyKetchup <maxdodon25@gmail.com>
2021-03-27 01:11:17 +0000

    Separator component

---
+++ src/components/Separator/index.js
@@ -0,0 +1,7 @@
+export default function Separator() {
+  return (
+    <div className="flex bg-white w-full" style={{ height: "2px" }}>
+      <div className="m-auto bg-gray-700 h-full w-96" />
+    </div>
+  );
+};
